b4e2b4604ac1b79841eb8bf86a8f3d11e2fdaa53
bagheera30 <rizkialfian30103@gmail.com>
2023-04-02 23:20:41 +0700

    menambahkan class kuliahMahasiswa_1302210112

---
+++ tpmodul7_kelompok_09/KuliahMahasiswa_1302210112.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static tpmodul7_kelompok_09.KuliahMahasiswa_1302210112;
+
+namespace tpmodul7_kelompok_09
+{
+    internal class KuliahMahasiswa_1302210112
+    {
+        public class Course
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class CoursesList
+        {
+            public List<Course> courses { get; set; }
+        }
+
+        public void ReadJSON()
+        {
+            string filePath = "C:\\Users\\asus\\Documents\\latihan\\tpkpl\\tpmodul7_kelompok_09\\tpmodul7_kelompok_09\\tp7_2_1302210112.json";
+            string jsonString = File.ReadAllText(filePath);
+            CoursesList coursesList = JsonSerializer.Deserialize<CoursesList>(jsonString);
+
+            Console.WriteLine("Daftar Mata Kuliah yang diambil:");
+            int i = 1;
+            foreach (Course course in coursesList.courses)
+            {
+                Console.WriteLine($"MK {i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+
+    }
+}
--- tpmodul7_kelompok_09/Program.cs
+++ tpmodul7_kelompok_09/Program.cs
@@ -4,3 +4,6 @@ using tpmodul7_kelompok_09;
 DataMahasiswa1302210112 data=new DataMahasiswa1302210112();
 data.readJson();
 Console.WriteLine();
+
+KuliahMahasiswa_1302210112 k=new KuliahMahasiswa_1302210112();
+k.ReadJSON();
