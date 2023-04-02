5a3796e8cb7a4a7255e45db2b9cf7dbebe7f1fda
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-04-02 23:23:26 +0700

    menambahkan class KuliahMahasiswa_1302210105

---
+++ tpmodul7_kelompok_09/KuliahMahasiswa_1302210105.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_09
+{
+    public class KuliahMahasiswa_1302210105
+    {
+        public List<MataKuliah_1302210105> matkul { get; set; }
+        public KuliahMahasiswa_1302210105(List<MataKuliah_1302210105> matkul)
+        {
+            this.matkul = matkul;
+        }
+        public KuliahMahasiswa_1302210105()
+        {
+        }
+    }
+    public class MataKuliah_1302210105
+    {
+        public string kode { get; set; }
+        public string nama { get; set; }
+        public MataKuliah_1302210105(string kodeX, string namaX)
+        {
+            kode = kodeX;
+            nama = namaX;
+        }
+        public MataKuliah_1302210105() { }
+    }
+}
--- tpmodul7_kelompok_09/Program.cs
+++ tpmodul7_kelompok_09/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using tpmodul7_kelompok_09;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa_1302210105 data = new DataMahasiswa_1302210105();
+        String jsonString = File.ReadAllText("..\\..\\..\\tp7_1_1302210105.json");
+        DataMahasiswa_1302210105 mhs = JsonSerializer.Deserialize<DataMahasiswa_1302210105>(jsonString);
+        Console.WriteLine("Nama: " + mhs.nama.depan + " " + mhs.nama.belakang + " dengan nim  " + mhs.nim + " dari fakultas " + mhs.fakultas);
+        String jsonString2 = File.ReadAllText("..\\..\\..\\tp7_2_1302210105.json");
+        KuliahMahasiswa_1302210105 kuliah = JsonSerializer.Deserialize<KuliahMahasiswa_1302210105>(jsonString2);
+        if (kuliah.matkul != null)
+        {
+            int i = 1;
+            foreach (MataKuliah_1302210105 matkul in kuliah.matkul)
+            {
+                Console.WriteLine("MK " + (i) + " " + matkul.kode + " - " + matkul.nama);
+                i++;
+            }
+        }
+    }
+
+}
